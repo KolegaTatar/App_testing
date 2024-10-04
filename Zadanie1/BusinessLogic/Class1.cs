1b2e01a070d94e6ef8eb807114faa334776a99f8
Wiktor Tatarynowicz <wiktor.tatarynowicz@gmail.com>
2024-10-04 17:39:03 +0200

    add lesson 1

---
+++ Zadanie1/BusinessLogic/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace BusinessLogic
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Kalkulator mający metody służące do dodawania i odejmowania
+        /// </summary>
+        /// <param name="x">pierwszy argument metody</param>
+        /// <param name="y">drugi argument metody</param>
+        /// <returns>x+y</returns>
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        /// <summary>
+        /// Kalkulator mający metody służące do dodawania i odejmowania
+        /// </summary>
+        /// <param name="x">pierwszy argument metody</param>
+        /// <param name="y">drugi argument metody</param>
+        /// <returns>x-y</returns>
+        public int Substract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLogic;
+namespace Zadanie1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Add(5, 8));
+            Console.WriteLine(calculator.Substract(9, 10));
+        }
+    }
+}
