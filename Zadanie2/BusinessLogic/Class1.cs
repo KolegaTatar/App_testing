67aefdf9b6167a3623cab6aa38ffe5d35ff3584c
Wiktor <wiktor.tatarynowicz@gmail.com>
2024-10-04 17:41:01 +0200

    add lesson 2

---
+++ Zadanie2/BusinessLogic/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace BusinessLogic
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Kalkulator mający metody służące do dodawania i odejmowania
+        /// </summary>
+        /// <param name="x">pierwszy argument metody</param>
+        /// <param name="y">drugi argument metody</param>
+        /// <returns>x+y</returns>
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        /// <summary>
+        /// Kalkulator mający metody służące do dodawania i odejmowania
+        /// </summary>
+        /// <param name="x">pierwszy argument metody</param>
+        /// <param name="y">drugi argument metody</param>
+        /// <returns>x-y</returns>
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+
+
+        /// <summary>
+        /// Kalkulator posiadający funkcje dzielenia
+        /// </summary>
+        /// <param name="x">pierwszy argument metody</param>
+        /// <param name="y">drugi argument metody</param>
+        /// <returns>x/y</returns>
+        public double Division(double x, double y)
+        {
+            return x / y;            
+        }
+
+
+
+        /// <summary>
+        /// Kalkulator posiadający funkcje mnożenia
+        /// </summary>
+        /// <param name="x">pierwszy argument metody</param>
+        /// <param name="y">drugi argument metody</param>
+        /// <returns>x/y</returns>
+        public double Multiplication(double x, double y)
+        {
+            return x * y;
+        }
+
+        /// <summary>
+        /// Kalkulator posiadający funkcje potęgowania
+        /// </summary>
+        /// <param name="x">pierwszy argument metody</param>
+        /// <param name="y">drugi argument metody</param>
+        /// <returns>x/y</returns>
+        public int ThePower(int x, int y)
+        {
+            
+            int result = x;
+            while (y > 1)
+            {
+                x = x * result;
+                y--;
+            }
+            return x;
+        }
+    }
+}
